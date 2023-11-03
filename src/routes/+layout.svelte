c49f6782fdc72741a4abc39f70ecbe55b0187e91
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-11-03 09:39:49 -0700

    Add donors json file content and fetch notes to layout page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
   /** @type {import('./$types').LayoutData} */
   export let data;
+  // export let jsonData;
+  // fetch('./data.json')
+  //   .then((response) => response.json())
+  //   .then((json) => json = jsonData);
 </script>
 
 <div>
