c2ba2601f14f762491323fe9d58c15777efe65d5
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-11-03 11:17:35 -0700

    Add seed file content and prisma seed script to package json file

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Install: `npm install typescript ts-node @types/node --save-dev`
 Initialize TypeScript: `npx tsc --init`
 Install Prisma CLI: `npm install prisma --save-dev`
 Setup Prisma: `npx prisma init --datasource-provider sqlite`
+Create data models in `prisma/schema.prisma`
+Create `seed.ts` file to seed data in `prisma` directory
---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+// import { categories } from "../src/lib/categories.json";
+// import { donations } from "../src/lib/categories.json";
+import { donors } from "../src/lib/categories.json";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    console.log(`Start seeding ...`);
+
+    for (const p of donors) {
+        const donor = await prisma.donor.create({
+            data: {
+                name: p.name,
+                email: p.email,
+                phone: p.phone,
+                donations: {
+                    create: {
+                        category: p.category,
+                        amount: p.amount,
+                        date: p.date
+                    },
+                },
+            }
+        });
+        console.log(`Created donor with id: ${donor.id}`);
+    }
+    console.log(`Seeding finished.`);
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
