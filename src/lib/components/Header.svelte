315e3212fc7d81cff6f53c705671444bc882dd72
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-11-06 16:20:32 -0800

    Add layout styling to header component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,12 +3,16 @@
   //   update this^ icon to fit theme
 </script>
 
-<header>
-  <hgroup>
-    <h1>the Sound Community Shelter</h1>
-    <p>donations management tool</p>
-  </hgroup>
-  <div>
-    <img class="logo" {src} alt="svelte icon" />
+<header class="site-header">
+  <div class="container">
+    <div class="site-header__inner">
+      <hgroup class="hgroup">
+        <h1>the Sound Community Shelter</h1>
+        <p>donations management tool</p>
+      </hgroup>
+      <div>
+        <img class="logo" {src} alt="svelte icon" />
+      </div>
+    </div>
   </div>
 </header>
