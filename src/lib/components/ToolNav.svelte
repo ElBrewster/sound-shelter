1f582583c9ba095d8c344f1d3198d64f0064ca68
ElBrewster <113723085+ElBrewster@users.noreply.github.com>
2023-11-07 09:02:27 -0800

    Add arrow symbol to tool nav

--- src/lib/components/ToolNav.svelte
+++ src/lib/components/ToolNav.svelte
@@ -1,10 +1,10 @@
 <div class="intra-page-nav">
   <div class="container flow">
     <nav class="nav flex-group">
-      <a href="#newDonation">Record a Donation</a>
-      <a href="#donorList">Recent Donors</a>
-      <a href="#inventory">Inventory Distribution</a>
-      <a href="#donationList">Recent Donations</a>
+      <a href="#newDonation">&#8594 Record a Donation</a>
+      <a href="#donorList">&#8594 Recent Donors</a>
+      <a href="#inventory">&#8594 Inventory Distribution</a>
+      <a href="#donationList">&#8594 Recent Donations</a>
     </nav>
   </div>
 </div>
